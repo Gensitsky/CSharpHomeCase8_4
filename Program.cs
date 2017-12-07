7a3530a09db7a37ee70a2edbddff9fba0936c317
Gensitsky <vadim.gensitsky@gmail.com>
2017-12-07 07:41:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+    Подсчитать, сколько раз каждое слово встречает-
+    ся в заданном тексте. Результат записать в коллекцию
+    Dictionary<TKey, TValue>.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHomeCase8_4
+{
+    /// <summary>
+    /// Assignment in work
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
